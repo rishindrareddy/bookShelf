31eab9618d59ddaa39fec936f15e3030a9fb85f4
rishindrareddy <rishindrareddy@hotmail.com>
2017-08-27 23:29:34 -0700

    new booklist component

---
+++ src/components/book_list.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+// renders the view of component that has list of books. 
+export default class BookList extends Component {
+
+  renderList(){
+    return this.props.books.map((book) => {
+      return (
+        <li key=(book.title) className="list-group-item">
+          {book.title}
+        </li>
+      )
+    })
+  }
+
+  render(){
+    return{
+      <ul className="List-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    }
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+// handles the book list.
+// Consider reducer as controller in angular
+// it handles data objects of the view.
+
+export default function() {
+  return[
+    {title: 'JavaScript intro'},
+    {title: 'JavaScript intermediate'},
+    {title: 'JavaScript expert'},
+    {title: 'JavaScript ultimate'}
+  ]
+}
